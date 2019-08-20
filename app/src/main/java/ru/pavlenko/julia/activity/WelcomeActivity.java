9b4b6fff46769d2bdfa5bcb7cfaeaaca48a92873
kruzyabra <kruzyabra@yandex.ru>
2019-08-20 04:35:29 +0300

    Add functional for welcome button.

--- app/src/main/java/ru/pavlenko/julia/activity/WelcomeActivity.java
+++ app/src/main/java/ru/pavlenko/julia/activity/WelcomeActivity.java
@@ -1,8 +1,11 @@
 package ru.pavlenko.julia.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +24,20 @@ public class WelcomeActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(new ViewPagerAdapter(getLayoutInflater()));
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        sharedPreferences.edit().putBoolean("show_welcome_screen", false);
+        Button welcomeButton = findViewById(R.id.welcome_button);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(WelcomeActivity.this);
+                sharedPreferences.edit().putBoolean("show_welcome_screen", false).apply();
+
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
