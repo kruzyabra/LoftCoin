1de6f7bbccf42e2a3637c192a0d77986331e7188
kruzyabra <kruzyabra@yandex.ru>
2019-08-24 19:18:49 +0300

    Add MainModelView.

--- app/src/main/java/ru/pavlenko/julia/activity/MainActivity.java
+++ app/src/main/java/ru/pavlenko/julia/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package ru.pavlenko.julia.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ru.pavlenko.julia.R;
+import ru.pavlenko.julia.model.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final MainViewModel viewModel = new MainViewModel();
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                viewModel.getSelectedId();
+                return true;
+            }
+        });
+
+        viewModel.getTitle().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String title) {
+                toolbar.setTitle(title);
+            }
+        });
+
+
     }
 }
