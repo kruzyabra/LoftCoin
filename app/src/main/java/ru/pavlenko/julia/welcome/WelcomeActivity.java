5ce4d1dbcea607b666b06266abdb788867fa1bbd
kruzyabra <kruzyabra@yandex.ru>
2019-09-22 15:45:09 +0300

    Fix welcome button style.

--- app/src/main/java/ru/pavlenko/julia/welcome/WelcomeActivity.java
+++ app/src/main/java/ru/pavlenko/julia/welcome/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,7 +29,7 @@ public class WelcomeActivity extends AppCompatActivity{
         CircleIndicator circleIndicator = findViewById(R.id.circle_indicator);
         circleIndicator.setViewPager(viewPager);
 
-        Button welcomeButton = findViewById(R.id.welcome_button);
+        TextView welcomeButton = findViewById(R.id.welcome_button);
         welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
